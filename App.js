529a1eb5ef48e333267985cc11c01086184ecee6
Pop Raul <50402381+PopRaulC@users.noreply.github.com>
2022-09-26 13:14:19 +0300

    main

---
+++ App.js
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import GoalItem from "./components/GoalItem";
+import { StyleSheet, View, FlatList } from "react-native";
+import GoalInput from "./components/GoalInput";
+
+export default function App() {
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  function addGoalHandler(enteredGoalText) {
+    setCourseGoals((currentCourseGoals) => [
+      ...courseGoals,
+      { text: enteredGoalText, id: Math.random().toString() },
+      // FLatList se uita in obiect si cauta daca are proprietatea de ley si o pune automat
+    ]);
+  }
+
+  return (
+    <View style={styles.appContainer}>
+      <GoalInput onAddGoal={addGoalHandler} />
+      <View style={styles.goalsContainer}>
+        <FlatList
+          data={courseGoals}
+          renderItem={(itemData) => {
+            return <GoalItem text={itemData.item.text} />;
+          }}
+          keyExtractor={(item, index) => {
+            return item.id;
+            // asa ii spunem la flatlist ca key este idul
+          }}
+          alwaysBounceVertical={false}
+        />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  appContainer: {
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16,
+  },
+  goalsContainer: {
+    flex: 5,
+  },
+});
