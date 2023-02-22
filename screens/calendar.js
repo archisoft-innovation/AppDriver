dd893aa8de46955ab5359e7de8704b0f6e82947d
Raul <pop_raul_93@yahoo.com>
2023-02-22 23:22:57 +0200

    set schedule

--- screens/calendar.js
+++ screens/calendar.js
@@ -12,6 +12,21 @@ export default function Calendar() {
   const [currentDate, setCurrentDate] = useState("");
   const [selectedDate, setSelectedDate] = useState(new Date());
   const [modalVisibility, setmodalVisibility] = useState(false);
+  const [ten, setTen] = useState(false);
+  const [eleven, seteleven] = useState(false);
+  const [twelve, setTwelve] = useState(false);
+  const [thirteen, setThirteen] = useState(false);
+  const [fourteen, setFourteen] = useState(false);
+  const [fifteen, setFifteen] = useState(false);
+  const [sixteen, setsixteen] = useState(false);
+  const [seventeen, setseventeen] = useState(false);
+  const [eighteen, seteighteen] = useState(false);
+  const [nineteen, setnineteen] = useState(false);
+  const [twenty, settwenty] = useState(false);
+  const [twentyOne, settwentyOne] = useState(false);
+  const [twentyTwo, settwentyTwo] = useState(false);
+  const [twentyThree, settwentyThree] = useState(false);
+  const [twentyFour, settwentyFour] = useState(false);
   useEffect(() => {
     var date = new Date().getDate(); //Current Date
     var month = new Date().getMonth() + 1; //Current Month
@@ -29,8 +44,55 @@ export default function Calendar() {
     setmodalVisibility(false);
     console.log(selectedDate);
   }
+
   function setModalVisible() {
     setmodalVisibility(true);
+  }
+
+  function switchTen() {
+    setTen((current) => !current);
+  }
+  function switchtwelve() {
+    setTwelve((current) => !current);
+  }
+  function switeleven() {
+    seteleven((current) => !current);
+  }
+  function switchthirteen() {
+    setThirteen((current) => !current);
+  }
+  function switchfourteen() {
+    setFourteen((current) => !current);
+  }
+  function switchfifteen() {
+    setFifteen((current) => !current);
+  }
+  function switchsixteen() {
+    setsixteen((current) => !current);
+  }
+  function switchseventeen() {
+    setseventeen((current) => !current);
+  }
+  function switcheighteen() {
+    seteighteen((current) => !current);
+  }
+  function switchnineteen() {
+    setnineteen((current) => !current);
+  }
+  function switchtwenty() {
+    settwenty((current) => !current);
+  }
+  function switchtwentyOne() {
+    settwentyOne((current) => !current);
+  }
+  function switchtwentyTwo() {
+    settwentyTwo((current) => !current);
+  }
+  function switchtwentyThree() {
+    settwentyThree((current) => !current);
+  }
+  function switchtwentyFour() {
+    settwentyFour((current) => !current);
   }
   return (
     <View style={styles.inputContainerMain}>
@@ -52,7 +114,7 @@ export default function Calendar() {
             }}
             // onSelectedChange={(newValue) => setSelectedDate(newValue)}
             onSelectedChange={(newValue) => test(newValue)}
-            current={currentDate}
+            // current={currentDate}
             // selected="2020-07-23"
             mode="calendar"
             // minuteInterval={30}
@@ -61,79 +123,158 @@ export default function Calendar() {
         </View>
       </Modal>
       <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={styles.viewSt}></View>
         <View style={styles.viewSt}>
           <View style={styles.presableBtn}>
-            <Pressable style={styles.firstPres}>
+            <Pressable
+              style={[styles.pres, ten ? styles.truePres : styles.falsePres]}
+              onPress={switchTen}
+            >
               <Text style={styles.pressText}> 10:00</Text>
             </Pressable>
-            <Pressable style={styles.falsePress}>
+            <Pressable
+              style={[styles.pres, eleven ? styles.truePres : styles.falsePres]}
+              onPress={switeleven}
+            >
               <Text style={styles.pressText}>11:00</Text>
             </Pressable>
           </View>
         </View>
         <View style={styles.viewSt}>
           <View style={styles.presableBtn}>
-            <Pressable style={styles.firstPres}>
+            <Pressable
+              style={[styles.pres, twelve ? styles.truePres : styles.falsePres]}
+              onPress={switchtwelve}
+            >
               <Text style={styles.pressText}> 12:00</Text>
             </Pressable>
-            <Pressable style={styles.falsePress}>
+            <Pressable
+              style={[
+                styles.pres,
+                thirteen ? styles.truePres : styles.falsePres,
+              ]}
+              onPress={switchthirteen}
+            >
               <Text style={styles.pressText}>13:00</Text>
             </Pressable>
           </View>
         </View>
         <View style={styles.viewSt}>
           <View style={styles.presableBtn}>
-            <Pressable style={styles.falsePress}>
+            <Pressable
+              style={[
+                styles.pres,
+                fourteen ? styles.truePres : styles.falsePres,
+              ]}
+              onPress={switchfourteen}
+            >
               <Text style={styles.pressText}> 14:00</Text>
             </Pressable>
-            <Pressable style={styles.firstPres}>
+            <Pressable
+              style={[
+                styles.pres,
+                fifteen ? styles.truePres : styles.falsePres,
+              ]}
+              onPress={switchfifteen}
+            >
               <Text style={styles.pressText}>15:00</Text>
             </Pressable>
           </View>
         </View>
         <View style={styles.viewSt}>
           <View style={styles.presableBtn}>
-            <Pressable style={styles.firstPres}>
+            <Pressable
+              style={[
+                styles.pres,
+                sixteen ? styles.truePres : styles.falsePres,
+              ]}
+              onPress={switchsixteen}
+            >
               <Text style={styles.pressText}> 16:00</Text>
             </Pressable>
-            <Pressable style={styles.falsePress}>
+            <Pressable
+              style={[
+                styles.pres,
+                seventeen ? styles.truePres : styles.falsePres,
+              ]}
+              onPress={switchseventeen}
+            >
               <Text style={styles.pressText}>17:00</Text>
             </Pressable>
           </View>
         </View>
         <View style={styles.viewSt}>
           <View style={styles.presableBtn}>
-            <Pressable style={styles.firstPres}>
+            <Pressable
+              style={[
+                styles.pres,
+                eighteen ? styles.truePres : styles.falsePres,
+              ]}
+              onPress={switcheighteen}
+            >
               <Text style={styles.pressText}> 18:00</Text>
             </Pressable>
-            <Pressable style={styles.firstPres}>
+            <Pressable
+              style={[
+                styles.pres,
+                nineteen ? styles.truePres : styles.falsePres,
+              ]}
+              onPress={switchnineteen}
+            >
               <Text style={styles.pressText}>19:00</Text>
             </Pressable>
           </View>
         </View>
         <View style={styles.viewSt}>
           <View style={styles.presableBtn}>
-            <Pressable style={styles.firstPres}>
+            <Pressable
+              style={[styles.pres, twenty ? styles.truePres : styles.falsePres]}
+              onPress={switchtwenty}
+            >
               <Text style={styles.pressText}> 20:00</Text>
             </Pressable>
-            <Pressable style={styles.firstPres}>
+            <Pressable
+              style={[
+                styles.pres,
+                twentyOne ? styles.truePres : styles.falsePres,
+              ]}
+              onPress={switchtwentyOne}
+            >
               <Text style={styles.pressText}>21:00</Text>
             </Pressable>
           </View>
         </View>
         <View style={styles.viewSt}>
           <View style={styles.presableBtn}>
-            <Pressable style={styles.falsePress}>
+            <Pressable
+              style={[
+                styles.pres,
+                twentyTwo ? styles.truePres : styles.falsePres,
+              ]}
+              onPress={switchtwentyTwo}
+            >
               <Text style={styles.pressText}> 22:00</Text>
             </Pressable>
-            <Pressable style={styles.falsePress}>
+            <Pressable
+              style={[
+                styles.pres,
+                twentyThree ? styles.truePres : styles.falsePres,
+              ]}
+              onPress={switchtwentyThree}
+            >
               <Text style={styles.pressText}>23:00</Text>
             </Pressable>
           </View>
         </View>
         <View style={styles.viewSt}>
           <View style={styles.presableBtn}>
-            <Pressable style={styles.firstPres}>
+            <Pressable
+              style={[
+                styles.pres,
+                twentyFour ? styles.truePres : styles.falsePres,
+              ]}
+              onPress={switchtwentyFour}
+            >
               <Text style={styles.pressText}>00:00</Text>
             </Pressable>
           </View>
@@ -156,7 +297,14 @@ export default function Calendar() {
 
         {/* <View style={styles.hoursViews}>
           <View style={styles.bordersBottom}>
-            <Pressable style={styles.firstPres}>
+            <Pressable
+              style={[
+                styles.pres,
+                testTrue ? styles.truePres : styles.falsePres,
+              ]}
+              onPress={switchTen}
+
+            >
               <Text style={styles.goalText}>08:00</Text>
             </Pressable>
           </View>
@@ -268,18 +416,31 @@ const styles = StyleSheet.create({
   presables: {
     padding: 6,
   },
-  firstPres: {
+  pres: {
     padding: 16,
-    backgroundColor: "green",
     borderRadius: 20,
     borderWidth: 1,
+  },
+  truePres: {
+    backgroundColor: "green",
+    borderColor: "green",
+  },
+  falsePres: {
+    backgroundColor: "red",
+    borderColor: "red",
+  },
+  firstPres: {
+    padding: 16,
+    borderRadius: 20,
+    borderWidth: 1,
+    backgroundColor: "green",
     borderColor: "green",
   },
   falsePress: {
     padding: 16,
-    backgroundColor: "red",
     borderRadius: 20,
     borderWidth: 1,
+    backgroundColor: "red",
     borderColor: "red",
   },
   pressText: {
